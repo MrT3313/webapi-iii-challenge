cdd2eac95258ffcfe51d550e8773f11a4aa20ccc
MrT3313 <rturgeon13@gmail.com>
2019-05-14 18:11:29 -0500

    morgan & helmet middleware added

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
-const express = 'express';
-
+const express = require('express');
+const helmet = require('helmet');
+const logger = require('morgan')
 const server = express();
+
+// GLOBAL MIDDLEWARE
+  //built in middleware
+    server.use(express.json());
+
+  //custom middleware
+    server.use(helmet());
+    server.use(logger('dev'))
+ 
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
-//custom middleware
+  // function logger(req, res, next) {
 
-function logger(req, res, next) {
+  // };
 
-};
 
 module.exports = server;
