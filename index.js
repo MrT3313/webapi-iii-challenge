36eead98093b11d243fa7a8526d8bd48302ef06a
MrT3313 <rturgeon13@gmail.com>
2019-05-16 13:46:34 -0500

    Heroku
    initial deployment attempt

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // code away!
 const server = require('./server.js')
 
-server.listen(4000, () => {
-    console.log('listening on port 4000')
+
+const PORT = process.env.PORT || 4000
+
+server.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
 })
